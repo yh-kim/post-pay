050ae6e9928043c12854cca294c184a469c0b86c
HaEun Kim <hagomy@naver.com>
2018-04-06 16:31:11 +0900

    [date] 2018.04.06 by HaEun [add] add SendingActivity
    [change]
    change colorPrimary

---
+++ app/src/main/kotlin/com/pickth/postpay/view/sending/SendingActivity.kt
@@ -0,0 +1,36 @@
+package com.pickth.postpay.view.sending
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import com.pickth.postpay.R
+import com.pickth.postpay.manager.SavingDataManager
+import kotlinx.android.synthetic.main.activity_sending.*
+import kotlinx.android.synthetic.main.base_toolbar.*
+import org.jetbrains.anko.toast
+
+/**
+ * Created by HaEun on 2018-04-06.
+ */
+class SendingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sending)
+
+        // actionbar
+        setSupportActionBar(toolbar_base)
+
+        btn_sending.setOnClickListener {
+            val per = SavingDataManager.getSavingPercentage(this)
+            if (edt_sending.text.isBlank()) {
+                toast("금액을 입력해주세요")
+            }else {
+                val sending = Integer.parseInt(edt_sending.text.toString())
+                var saving = sending * per / 100
+                SavingDataManager.setSavingMoney(applicationContext, SavingDataManager.getSavingMoney(applicationContext) + saving)
+                finish()
+            }
+        }
+    }
+}
